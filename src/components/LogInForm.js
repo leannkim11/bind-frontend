de822cd3731d43ccc4906dabb16788a09037c78b
Leann Kim <leannminakim@gmail.com>
2018-09-06 16:49:51 -0400

    in progress

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter, Redirect } from "react-router";
+import { withRouter, Redirect, Link } from "react-router";
 import { loginUser } from "../actions/user";
 
 export default class LogInForm extends Component {
@@ -9,36 +9,63 @@ export default class LogInForm extends Component {
     password: ""
   };
 
+  handleFormSubmit = e => {
+    e.preventDefault();
+    console.log("login button clicked");
+
+    fetch("http://localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        password: "leannleann",
+        email: "leann@leann.com"
+      })
+    })
+      .then(res => res.json())
+      .then(json => localStorage.setItem("token", json.jwt));
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
     return (
       <React.Fragment>
         <img src="./bind-logo.svg" alt="" id="mainlogo" className="center" />
-        <form id="sign-up-form" action="/loggedin">
+        <form
+          id="sign-up-form"
+          action="/loggedin"
+          onSubmit={this.handleFormSubmit}
+        >
           <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label>Email address</label>
             <input
-              col-5
-              type="email"
+              name="email"
               className="form-control"
               id="exampleInputEmail1"
-              aria-describedby="emailHelp"
               placeholder="Enter email"
+              value={this.state.email}
+              onChange={this.handleChange}
             />
           </div>
           <div className="form-group">
             <label for="exampleInputPassword1">Password</label>
             <input
-              col-5
-              type="password"
+              name="password"
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
             />
           </div>
 
-          <button type="submit" className="btn btn-primary">
+          <a href="/loggedin" type="submit" className="btn btn-primary">
             Submit
-          </button>
+          </a>
 
           <br />
           <br />
